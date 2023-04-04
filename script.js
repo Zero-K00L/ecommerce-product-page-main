1ec2813e867c6b8c476e1577a7c51b83f161921c
John Grudzinski <johngrudzinski@yahoo.com>
2023-04-04 15:54:34 -0400

    Sixth Commit

--- script.js
+++ script.js
@@ -38,3 +38,26 @@ plusBtn.addEventListener('click', function(){
     }
 });
 
+// adds number of product selected to the cart when "add to cart" button is pressed. Number of product selected will be representded on cart icon
+
+const cartBtn = document.querySelector('.cart-btn');
+const cartCounter = document.querySelector('.cart-counter');
+const emptyCart = document.querySelector('.cart-display-empty');
+const fullCart = document.querySelector('.cart-full-container');
+const cartQuantity = document.querySelector('.cart-item-amount');
+const cartTotal = document.querySelector('.cart-total-price');
+
+
+cartBtn.addEventListener('click',function() {
+    if(productAmount.innerText == 0) {
+        cartCounter.classList.add('hidden');
+    }
+    else if(productAmount.innerText > 0) {
+        cartCounter.innerText = productAmount.innerText;
+        cartCounter.classList.remove('hidden');
+        emptyCart.classList.add('hidden');
+        fullCart.classList.remove('hidden');
+        cartQuantity.innerText = cartCounter.innerText;
+        cartTotal.innerText = '  $' + 125.00 * cartQuantity.innerText + ".00";
+    }
+});
